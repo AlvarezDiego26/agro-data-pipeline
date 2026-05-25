4a8d7d585b4c82ab382862cd5604a1056c245c4b
AlvarezDiego26 <diego.alvarez@tecsup.edu.pe>
2026-05-25 05:30:05 -0500

    Limpia procedencia en ceros historicos de precios

--- ingesta-datos/sisap/src/sisap_light/jobs/common.py
+++ ingesta-datos/sisap/src/sisap_light/jobs/common.py
@@ -1089,14 +1089,13 @@ def _build_historical_zero_frame_volumen(query) -> pl.DataFrame:
 
 def _build_historical_zero_frame_precios(query) -> pl.DataFrame:
     fechas = _build_query_dates(query)
-    procedencia = query.procedencia_nombre or 'TODOS'
     rows = [
         {
             'fecha': fecha,
             'producto_codigo': query.producto_codigo,
             'producto_nombre': query.producto_nombre,
             'variedad': query.producto_nombre,
-            'procedencia': procedencia,
+            'procedencia': '',
             'procedencia_filtro_codigo': query.procedencia_codigo or '000000',
             'procedencia_filtro_nombre': query.procedencia_nombre or 'TODOS',
             'mercado_codigo': query.mercado_codigo or '',
